3662268d41631635ae1d9e6d6d90607b011c7e39
MARIANA <105300441+blackchase@users.noreply.github.com>
2024-08-01 18:06:28 -0300

    Update script.js

--- IA-main/script.js
+++ IA-main/script.js
@@ -9,12 +9,12 @@ const perguntas = [
         enunciado: "Qual é a principal diferença entre inteligência artificial fraca e forte",
         alternativas: [
             {
-                texto: "Correta",
-                afirmacao: "A inteligência artificial fraca é especializada em tarefas específicas, enquanto a inteligência artificial forte pode lidar com uma ampla gama de tarefas cognitivas como um ser humano. "
+                texto: "A inteligência artificial fraca é especializada em tarefas específicas, enquanto a inteligência artificial forte pode lidar com uma ampla gama de tarefas cognitivas como um ser humano.",
+                afirmacao: "Correta "
             },
             {
-                texto: " Incorreta",
-                afirmacao: "A inteligência artificial fraca é mais avançada tecnologicamente do que a inteligência artificial forte.a",
+                texto: " A inteligência artificial fraca é mais avançada tecnologicamente do que a inteligência artificial forte.",
+                afirmacao: "Incorreta",
             }
         ]
     },
@@ -23,12 +23,12 @@ const perguntas = [
         alternativas: [
             {
                 
-                texto: "Incorreta.",
-                afirmacao: "Classificado, agrupado e reforçado.",
+                texto: "Classificado, agrupado e reforçado.",
+                afirmacao: "Incorreta",
             },
             {
-                texto: "Correta.",
-                afirmacao: "Supervisionado, não supervisionado e por reforço.",
+                texto: "Supervisionado, não supervisionado e por reforço.",
+                afirmacao: "Correta",
             }
         ]
     },
@@ -36,12 +36,12 @@ const perguntas = [
         enunciado: "Como a inteligência artificial aprende no método supervisionado?",
         alternativas: [
             {
-                texto: "Correta.",
-                afirmacao: "Aprendendo a partir de dados rotulados, onde o algoritmo recebe entradas e saídas correspondentes para aprender a prever ou classificar novos dados.."
+                texto: "Aprendendo a partir de dados rotulados, onde o algoritmo recebe entradas e saídas correspondentes para aprender a prever ou classificar novos dados.",
+                afirmacao: "Correta."
             },
             {
-                texto: "Incorreta.",
-                afirmacao: "Aprendendo a partir de dados não rotulados, onde o algoritmo tenta encontrar padrões sem exemplos anteriores.",
+                texto: "Aprendendo a partir de dados não rotulados, onde o algoritmo tenta encontrar padrões sem exemplos anteriores.",
+                afirmacao: "Incorreta.",
             }
         ]
     },
@@ -49,12 +49,12 @@ const perguntas = [
         enunciado: "Quais são algumas preocupações éticas associadas à inteligência artificial?",
         alternativas: [
             {
-                texto: "Incorreta.",
-                afirmacao: "Apenas questões técnicas relacionadas ao desempenho dos algoritmos.",
+                texto: "Apenas questões técnicas relacionadas ao desempenho dos algoritmos.",
+                afirmacao: "Incorreto",
             },
             {
-                texto: "Correta.",
-                afirmacao: "Viés nos dados, privacidade e segurança dos dados, impacto socioeconômico e ética do uso de IA em decisões críticas.",
+                texto: "Viés nos dados, privacidade e segurança dos dados, impacto socioeconômico e ética do uso de IA em decisões críticas.",
+                afirmacao: "Correta",
             }
         ]
     },
@@ -62,12 +62,12 @@ const perguntas = [
         enunciado: "Quais setores estão se beneficiando mais da aplicação de inteligência artificial? ",
         alternativas: [
             {
-                texto: "Incorreta.",
-                afirmacao: "Apenas setores industriais tradicionais estão se beneficiando da IA.",
+                texto: "Apenas setores industriais tradicionais estão se beneficiando da IA.",
+                afirmacao: "Incorreta.",
             },
             {
-                texto: "Correta.",
-                afirmacao: "Saúde, finanças, varejo, transporte, entre outros.",
+                texto: "Saúde, finanças, varejo, transporte, entre outros.",
+                afirmacao: "Correta",
             }
         ]
     },
